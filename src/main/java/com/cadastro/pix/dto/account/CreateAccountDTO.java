a19e3e15cc97c73fd3f1b2497d74046a6043ceac
Rodrigo N Santos <rodnunes@uolinc.com>
2024-06-24 01:35:59 -0300

    improving validations

--- src/main/java/com/cadastro/pix/dto/account/CreateAccountDTO.java
+++ src/main/java/com/cadastro/pix/dto/account/CreateAccountDTO.java
@@ -1,5 +1,6 @@
 package com.cadastro.pix.dto.account;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +10,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateAccountDTO {
+    @NotNull
+    @Pattern(regexp = "^(corrente|poupança)$", message = "Invalid account type")
     private String accountType;
+
+    @NotNull
+    @Min(value = 1, message = "Invalid agency number")
+    @Max(value = 9999, message = "Invalid agency number")
     private Integer agencyNumber;
+
+    @NotNull
+    @Min(value = 1, message = "Invalid account number")
+    @Max(value = 99999999, message = "Invalid account number")
     private Integer accountNumber;
+
+    @Size(max = 14, message = "Identification number too long")
     private String identification;
 }
--- src/main/java/com/cadastro/pix/dto/pixKey/CreatePixKeyDTO.java
+++ src/main/java/com/cadastro/pix/dto/pixKey/CreatePixKeyDTO.java
@@ -1,5 +1,9 @@
 package com.cadastro.pix.dto.pixKey;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +13,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreatePixKeyDTO {
+
+    @NotNull
+    @Column(name = "key_type", nullable = false, length = 9)
     private String keyType;
+
+    @NotNull
+    @Column(name = "key_value", nullable = false, length = 77)
     private String keyValue;
+
+    @NotNull
+    @Min(value = 1, message = "Invalid agency number")
+    @Max(value = 9999, message = "Invalid agency number")
     private Integer agencyNumber;
+
+    @NotNull
+    @Min(value = 1, message = "Invalid account number")
+    @Max(value = 99999999, message = "Invalid account number")
     private Integer accountNumber;
 }
