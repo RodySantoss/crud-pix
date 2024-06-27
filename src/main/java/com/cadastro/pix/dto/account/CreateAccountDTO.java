869844173a91f44f0fd4af6ead04f19eeb0f3fde
Rodrigo N Santos <rodnunes@uolinc.com>
2024-06-27 00:30:37 -0300

    Alterando padrao de mensagens de validaçao

--- src/main/java/com/cadastro/pix/dto/account/CreateAccountDTO.java
+++ src/main/java/com/cadastro/pix/dto/account/CreateAccountDTO.java
@@ -10,21 +10,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateAccountDTO {
-    @NotNull(message = "accountType must not be null")
+    @NotNull(message = "Account type must not be null")
     @Pattern(regexp = "^(corrente|poupança)$", message = "Invalid account type")
     private String accountType;
 
-    @NotNull(message = "agencyNumber must not be null")
+    @NotNull(message = "Agency number must not be null")
     @Min(value = 1, message = "Invalid agency number")
     @Max(value = 9999, message = "Invalid agency number")
     private Integer agencyNumber;
 
-    @NotNull(message = "accountNumber must not be null")
+    @NotNull(message = "Account number must not be null")
     @Min(value = 1, message = "Invalid account number")
     @Max(value = 99999999, message = "Invalid account number")
     private Integer accountNumber;
 
-    @NotNull(message = "identification must not be null")
+    @NotNull(message = "Identification must not be null")
     @Size(max = 14, message = "Identification number too long")
     private String identification;
 }
